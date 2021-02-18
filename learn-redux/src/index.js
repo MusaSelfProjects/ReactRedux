357c2a935060257c275cc37ad9d6f1730cb70ba1
luffyUnleashed <37274365+luffyUnleashed@users.noreply.github.com>
2021-02-18 05:58:29 +0200

    finished

--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux'
+import allReducer from './reducers'
+import {Provider} from 'react-redux'
+const store=createStore(allReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+// Store -> Globlaized state 
+
+
+//Action -> name of function you would user 
+
+
+///Reducer 
+
+
+
+//Display in console
+
+//Dispatch call for action 
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+   <Provider store={store}>
+   <App />
+   </Provider> 
   </React.StrictMode>,
   document.getElementById('root')
 );
